207b406dc1f1b30975eb350845e5dced8f4d6050
gsarrouh <ghassan.sarrouh@gmail.com>
2020-07-04 17:20:45 -0400

    added new diagnostic flag "adams_flag", adds summary tables to all sections

--- main_project_file.py
+++ main_project_file.py
@@ -66,22 +66,47 @@ import matplotlib.pyplot as plt
 ## DEFINITIONS
 #
 ## MAY NEED TO EDIT: hard code the cluster membership definition cuts if not running Variational Analysis
-z_cutoff = [0.04,0.05]
+z_cutoff = [0.01,0.06]
 #
 #
 ## SECIONT (0.1): FLAGS
 ## MAY NEED TO EDIT: the following flags allow you to choose which sections (i.e. which progams) to execute (e.g. for testing purposes, you may want to run "master_data*.py" once to load the data, but not again on subsequent executions of the program for debugging)
 #
-##       0=off, skip section;     1=on, execute section
+## ADAM's flag
+adams_flag = 1                     # diagnostic summary table to account for every object given a redshift cut
+#                                  #  0=off;  1=on
 #
 ## PROJECT DIAGNOSTIC FLAG
 project_diagnostic_flag = 2        # 0=off, turn OFF ALL diagnostic flags in all files;    1=on, turn ON ALL diagnostics for every file that will be executed; or go into each individual program and turn on specific diagnostic flags by searching "diag_flag";      2=on, allow INDIVIDUAL diagnostic flags within separate programs to be turned on/off
+#
+#
+## PROJECT DIAGNOSTIC FLAG
+project_master_variational_flag = 0        # 0=off, don't perform variational analaysis;  1=on, do it. the analysis may also be turned on/off within "master_data*.py"
+#
+#
+##       0=off, skip section;     1=on, execute section
+#
 #
 section_1_flag = 1                 # data prep
 section_2_flag = 0                 # z plots
 section_3_flag = 0                 # limiting mass
 section_4_flag = 0                 # SMF
 #    
+## Update the user on what this program will run
+#
+print('"main_project_file" will run the following:')
+#
+if section_1_flag == 1:
+    print('Section 1: "master_data*.py"')
+#
+if section_2_flag == 1:
+    print('Section 2: "master_zplots*.py"')
+#
+if section_3_flag == 1:
+    print('Section 3: "data_mass_completeness*.py"')
+#
+if section_4_flag == 1:
+    print('Section 4: "master_smf*.py"')
 #
 ## SECTION (1): main DATA preparation file; imports raw data, sorts it by data type (i.e. photometric/spectroscopic data, stars, etc...), and classifies all galaxies with good photometric redshift estimates as Star-Forming (SF) or Quiescent (Q); it then runs a VARIATIONAL ANALYSIS to determine optimal redshift definitions (redshift "cuts") for cluster membership, based on which cuts yield an equal number of false positives/negatives in each mass bin; executes redshift cuts, classifies SF/Q galaxies as either cluster members, false pos/neg, or field galaxy; and finally, checks the catalogue for Brightest Cluster Galaxies (bCGs); MAIN program: master_data_7_final.py; SUB-programs: spec_completeness_binning.py, correction_factors.py;
 
@@ -128,8 +153,10 @@ if section_4_flag == 1:
 #
 #
 #
-## SECTION (5): 
+## SECTION (5): Prepare summary tables for ADAMS_FLAG to account for every object
 #
+if adams_flag == 1:
+    pass
 #
 #
 #
