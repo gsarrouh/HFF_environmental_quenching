412ed67f2a79cfa7271334e21ce94655f2685b44
gsarrouh <ghassan.sarrouh@gmail.com>
2020-07-10 10:41:56 -0400

    separated field sample for 0.3 < z < 0.55

--- spec_membership_selection_file.py
+++ spec_membership_selection_file.py
@@ -15,6 +15,7 @@ import numpy as np
 # 
 mem_spec = np.array([[0]*6]*2)       # initialize arrays to track cluster members, field, false pos/neg by cluster
 field_spec = np.array([[0]*6]*2)     # row_1=SF; row_2=Q; for all arrays
+far_field_spec = np.array([[0]*6]*2)
 pos_spec = np.array([[0]*6]*2)
 neg_spec = np.array([[0]*6]*2)
 other_member_spec = np.array([[0]*6]*2)                # track objects outside of (phot + spec) subsample
@@ -25,10 +26,17 @@ for counter in range(len(master_cat)):
     if master_cat['sub'][counter] == 1:                   # sub=1 identifies subsample with both spec & phot
         if master_cat['type'][counter]==1:                # type=1 identifies SF sample
             if abs(master_cat['z_clusterspec'][counter]) > z_cutoff_field[0] and abs(master_cat['z_clusterphot'][counter]) > z_cutoff_field[1]: 
-                master_cat['member'][counter] = 1         # member=1 for FIELD
-                for ii in range(len(field_spec[0])):
-                    if master_cat['cluster'][counter] == (ii+1):  # keep track of field objects by cluster
-                        field_spec[0][ii]+=1
+                if master_cat['z_peak'][counter] > z_field_bounds[0] and master_cat['z_peak'][counter] < z_field_bounds[1]:
+                    master_cat['member'][counter] = 1         # member=1 for FIELD
+                    for ii in range(len(field_spec[0])):
+                        if master_cat['cluster'][counter] == (ii+1):  # keep track of field objects by cluster
+                            field_spec[0][ii]+=1
+                else:
+                    master_cat['member'][counter] = 4         # member=4 for FIELD
+                    for ii in range(len(far_field_spec[0])):
+                        if master_cat['cluster'][counter] == (ii+1):  # keep track of field objects by cluster
+                            far_field_spec[0][ii]+=1
+                
             elif abs(master_cat['z_clusterspec'][counter]) > z_cutoff[0] and abs(master_cat['z_clusterphot'][counter]) < z_cutoff[1]: #SF_cutoff[1]: #
                 master_cat['member'][counter] = 2         # member=2 for FALSE POSITIVE
                 for ii in range(len(pos_spec[0])):
@@ -50,10 +58,16 @@ for counter in range(len(master_cat)):
                         lost_due_to_buffer_spec[0][ii]+=1
         elif master_cat['type'][counter]==2:                # type=2 identifies Q sample
             if abs(master_cat['z_clusterspec'][counter]) > z_cutoff_field[0] and abs(master_cat['z_clusterphot'][counter]) > z_cutoff_field[1]: 
-                master_cat['member'][counter] = 1         # member=1 for FIELD
-                for ii in range(len(field_spec[1])):
-                    if master_cat['cluster'][counter] == (ii+1):  # keep track of field objects by cluster
-                        field_spec[1][ii]+=1
+                if master_cat['z_peak'][counter] > z_field_bounds[0] and master_cat['z_peak'][counter] < z_field_bounds[1]:
+                    master_cat['member'][counter] = 1         # member=1 for FIELD
+                    for ii in range(len(field_spec[1])):
+                        if master_cat['cluster'][counter] == (ii+1):  # keep track of field objects by cluster
+                            field_spec[1][ii]+=1
+                else:
+                    master_cat['member'][counter] = 4         # member=4 for FIELD
+                    for ii in range(len(far_field_spec[1])):
+                        if master_cat['cluster'][counter] == (ii+1):  # keep track of field objects by cluster
+                            far_field_spec[1][ii]+=1
             elif abs(master_cat['z_clusterspec'][counter]) > z_cutoff[0] and abs(master_cat['z_clusterphot'][counter]) < z_cutoff[1]: #Q_cutoff[1]: #
                 master_cat['member'][counter] = 2         # member=2 for FALSE POSITIVE
                 for ii in range(len(pos_spec[1])):
